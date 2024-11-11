19ddb65bf753f6e2d058a6d97ce4decb6ce58e9c
Nathan Ford <nfor6010@uni.sydney.edu.au>
2024-11-12 10:53:04 +1100

    Add About component to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Hero from './components/Hero';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Hero />
-     
+      <About />
     </div>
   );
 }
