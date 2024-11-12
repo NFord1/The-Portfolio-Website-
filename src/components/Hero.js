1936ca8c71e65c5a21740e4a5fd43fda166d8cff
Nathan Ford <nfor6010@uni.sydney.edu.au>
2024-11-12 21:14:11 +1100

    Updated content of information in hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,9 +11,9 @@ const Hero = () => {
     return(
         <section id='hero' style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '2rem'}}>
             <div className='hero-text' style={{maxWidth: '50%'}}>
-                <h1>Hi, I'm Nathan Ford</h1>
-                <h2>Backend Developer and Full Stack Engineer</h2>
-                <p>Building scalable backend solutions for modern applications.</p>
+                <h1>Nathan Ford</h1>
+                <h2>Software Engineer - Full Stack</h2>
+                <p>Background in Physics & Mechatronic Engineering</p>
                 <button onClick={() => document.getElementById('projects').scrollIntoView({ behavior: 'smooth'})}>
                     View My Work
                 </button>
@@ -21,6 +21,7 @@ const Hero = () => {
             <div className='hero-chatbot'>
                 {/*Placeholder for Chatbot*/}
                 <img src={AIBubble} alt='AI Bubble' className='chatbot-gif' />
+                <p>Welcome! Curious about my projects? I’m here to guide you through my portfolio and answer any questions along the way. I'm a work in progress, but I guess for a software engineer, such is life! Let’s dive in!</p>
                 <input
                     type='text'
                     value={inputText}
