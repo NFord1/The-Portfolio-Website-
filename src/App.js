9cad6d91cde15abca2e7e9fa3adf13f58f124d14
Nathan Ford <nfor6010@uni.sydney.edu.au>
2024-11-12 15:31:07 +1100

    Add navbar to app

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Hero />
       <About />
       <Projects />
