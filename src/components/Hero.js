c4e883dce6c6d08e2b1c803b874a38ecc713f866
Nathan Ford <nfor6010@uni.sydney.edu.au>
2024-11-12 21:43:13 +1100

    Add contact button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,17 +9,25 @@ const Hero = () => {
     };
 
     return(
-        <section id='hero' style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '2rem'}}>
+        <section id='hero' >
             <div className='hero-text' style={{maxWidth: '50%'}}>
                 <h1>Nathan Ford</h1>
                 <h2>Software Engineer - Full Stack</h2>
                 <p>Background in Physics & Mechatronic Engineering</p>
-                <button onClick={() => document.getElementById('projects').scrollIntoView({ behavior: 'smooth'})}>
-                    View My Work
-                </button>
+                {/* Button Group */}
+                <div className='hero-buttons'>
+                    <button className='view-work-button' onClick={() => document.getElementById('projects').scrollIntoView({ behavior: 'smooth'})}>
+                        View My Work
+                    </button>
+
+                    <a href='mailto:nathanford333@gmail.com' className='contact-button'>
+                        Get in Touch
+                    </a>
+
+                </div>
+                
             </div>
             <div className='hero-chatbot'>
-                {/*Placeholder for Chatbot*/}
                 <img src={AIBubble} alt='AI Bubble' className='chatbot-gif' />
                 <p>Welcome! Curious about my projects? I’m here to guide you through my portfolio and answer any questions along the way. I'm a work in progress, but I guess for a software engineer, such is life! Let’s dive in!</p>
                 <input
