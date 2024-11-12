4330c30b63701453f2ec2c1150f00dca7c2c4348
Nathan Ford <nfor6010@uni.sydney.edu.au>
2024-11-12 11:20:56 +1100

    Add projects component to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Hero from './components/Hero';
 import About from './components/About';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <About />
+      <Projects />
     </div>
   );
 }
